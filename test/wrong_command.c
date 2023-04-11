53aa6314f61730630b437f0d432977e75cb81520
Steven Xu <stevenxxiu@gmail.com>
2023-04-11 17:12:30 +1000

    refactor: *Python 2* is obsolete now, so just use `python` instead of `python3`, `pip` instead of `pip3`

--- test/wrong_command.c
+++ test/wrong_command.c
@@ -3,7 +3,7 @@
 int main(void) {
     fprintf(stderr, "\x1B[31m\e[1m"
         "This is not the command you are looking for.\n"
-        "You probably want to run 'python3 -m pytest test/' instead"
+        "You probably want to run 'python -m pytest test/' instead"
         "\e[0m\n");
     return 1;
 }
