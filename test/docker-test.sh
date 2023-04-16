684d1e90df950b69470d83b578750ea2dc2c1cb6
Steven Xu <stevenxxiu@gmail.com>
2023-04-16 17:54:56 +1000

    test: add *Docker* config

---
+++ test/docker-test.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+set -o xtrace
+set -e
+
+export DOCKER_HOST="unix://${XDG_RUNTIME_DIR}/docker.sock"
+
+container_name=sshfs_container
+image_name=sshfs_image
+dir=$(dirname "$0")
+
+docker container stop $container_name || true
+docker container rm $container_name || true
+
+docker container run \
+    --detach \
+    --rm \
+    --interactive \
+    --name $container_name \
+    --mount type=bind,source="${dir}",target=/tmp/sshfs/test/ \
+    --cap-add SYS_ADMIN \
+    --device /dev/fuse \
+    $image_name
+
+docker exec $container_name mkdir /tmp/sshfs/build/
+docker cp $dir/../build/sshfs $container_name:/tmp/sshfs/build/
+
+docker exec $container_name sh -c '/usr/bin/sshd -Dp 22 &'
+docker exec --workdir /tmp/sshfs/ $container_name python -m pytest --numprocesses 10 test/
+
+docker container stop $container_name
