210d9bcf2fc8393bb6ff22b7485e02e14315df13
Steven Xu <stevenxxiu@gmail.com>
2023-04-18 23:17:41 +1000

    feat: handle server's `SSH_FX_DIR_NOT_EMPTY` response on calls to `sshfs_rmdir()`

--- sshfs.c
+++ sshfs.c
@@ -154,6 +154,29 @@
 #define SSH_FX_NO_CONNECTION                6
 #define SSH_FX_CONNECTION_LOST              7
 #define SSH_FX_OP_UNSUPPORTED               8
+#define SSH_FX_INVALID_HANDLE               9
+#define SSH_FX_NO_SUCH_PATH                 10
+#define SSH_FX_FILE_ALREADY_EXISTS          11
+#define SSH_FX_WRITE_PROTECT                12
+#define SSH_FX_NO_MEDIA                     13
+#define SSH_FX_NO_SPACE_ON_FILESYSTEM       14
+#define SSH_FX_QUOTA_EXCEEDED               15
+#define SSH_FX_UNKNOWN_PRINCIPAL            16
+#define SSH_FX_LOCK_CONFLICT                17
+#define SSH_FX_DIR_NOT_EMPTY                18
+#define SSH_FX_NOT_A_DIRECTORY              19
+#define SSH_FX_INVALID_FILENAME             20
+#define SSH_FX_LINK_LOOP                    21
+#define SSH_FX_CANNOT_DELETE                22
+#define SSH_FX_INVALID_PARAMETER            23
+#define SSH_FX_FILE_IS_A_DIRECTORY          24
+#define SSH_FX_BYTE_RANGE_LOCK_CONFLICT     25
+#define SSH_FX_BYTE_RANGE_LOCK_REFUSED      26
+#define SSH_FX_DELETE_PENDING               27
+#define SSH_FX_FILE_CORRUPT                 28
+#define SSH_FX_OWNER_INVALID                29
+#define SSH_FX_GROUP_INVALID                30
+#define SSH_FX_NO_MATCHING_BYTE_RANGE_LOCK  31
 
 #define SSH_FXF_RENAME_OVERWRITE            0x00000001
 #define SSH_FXF_RENAME_ATOMIC               0x00000002
@@ -1858,6 +1881,7 @@ static int sftp_error_to_errno(uint32_t error)
     case SSH_FX_NO_CONNECTION:     return ENOTCONN;
     case SSH_FX_CONNECTION_LOST:   return ECONNABORTED;
     case SSH_FX_OP_UNSUPPORTED:    return EOPNOTSUPP;
+    case SSH_FX_DIR_NOT_EMPTY:     return ENOTEMPTY;
     default:                       return EIO;
     }
 }
