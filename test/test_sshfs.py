70bbe373f72b6c4ee886f645555b39076609b7dd
Steven Xu <stevenxxiu@gmail.com>
2023-05-07 21:10:51 +1000

    test: add type of `self`

--- test/test_sshfs.py
+++ test/test_sshfs.py
@@ -9,6 +9,7 @@ import stat
 import sys
 from pathlib import Path
 from tempfile import NamedTemporaryFile
+from typing import Self
 
 import pytest
 from conftest import DataFile, SshfsDirs
@@ -21,7 +22,7 @@ pytestmark = fuse_test_marker()
 class NameGenerator:
     counter = 0
 
-    def __call__(self) -> str:
+    def __call__(self: Self) -> str:
         self.counter += 1
         return f'testfile_{self.counter}'
 
